62d71d99df7d79db208400bec49e92517b0615d0
Daniel Holth <208018+dholth@users.noreply.github.com>
2026-07-10 22:37:41 -0400

    improve Python template generator; enable "python -m zinescape ..."

--- setup.py
+++ setup.py
@@ -4,18 +4,13 @@ except ImportError:
     from distutils.core import setup
 
 config = {
-    'name': 'zinescape',
-    'author': 'Thaddeus Hughes',
-    'author_email': 'hughes.thad@gmail.com',
-    'version': '1.0-rc',
-    'install_requires': ['pypdf', 'argparse'],
-    'packages': ['zinescape'],
-
-    'entry_points': {
-        'console_scripts': {
-            'zinescape = zinescape.zinescape:cli'
-        }
-    }
+    "name": "zinescape",
+    "author": "Thaddeus Hughes",
+    "author_email": "hughes.thad@gmail.com",
+    "version": "1.0-rc",
+    "install_requires": ["pypdf", "argparse"],
+    "packages": ["zinescape"],
+    "entry_points": {"console_scripts": {"zinescape = zinescape.zinescape:cli"}},
 }
 
 setup(**config)
--- zinescape/zinescape.py
+++ zinescape/zinescape.py
@@ -1,94 +1,90 @@
-from pypdf import PdfWriter, PdfReader
-from pypdf.generic import RectangleObject
-import argparse
+"""
+Generate an inkscape template to author a zine, or compile a multi-page document
+into a zine.
+"""
 
-#A4_WIDTH = 595.28  # A4 width in points
-#A4_HEIGHT = 841.89  # A4 height in points
+import argparse
+import xml.etree.ElementTree as ET
+
+from pypdf import PdfReader, PdfWriter
+from pypdf.generic import RectangleObject
+
+# A4_WIDTH = 595.28  # A4 width in points
+# A4_HEIGHT = 841.89  # A4 height in points
 
 LETTER_WIDTH = 612
 LETTER_HEIGHT = 792
 
+
 def template(f_name, n_pages, width, height):
 
     width *= 96
-    height *= 96 # 8.5
-    spacing = 96 #1.0
-    margin = 48 #0.5
+    height *= 96  # 8.5
+    spacing = 96  # 1.0
+    margin = 48  # 0.5
 
-    doc = """<?xml version="1.0" encoding="UTF-8" standalone="no"?>
-    <svg
-       width="%d"
-       height="%d"
-       viewBox="0 0 %d %d"
-       version="1.1"
-       id="svg5"
-       inkscape:version="1.2.2 (b0a8486541, 2022-12-01)"
-       xml:space="preserve"
-       xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
-       xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
-       xmlns="http://www.w3.org/2000/svg"
-       xmlns:svg="http://www.w3.org/2000/svg"><defs
-         id="defs21" /><sodipodi:namedview
-         id="namedview7"
-         pagecolor="#ffffff"
-         bordercolor="#666666"
-         borderopacity="1.0"
-         inkscape:pageshadow="2"
-         inkscape:pageopacity="0.0"
-         inkscape:pagecheckerboard="0"
-         inkscape:document-units="in"
-         inkscape:snap-global="false"
-         fit-margin-top="0"
-         fit-margin-left="0"
-         fit-margin-right="0"
-         fit-margin-bottom="0"
-         units="in"
-         width="%din"
-         inkscape:showpageshadow="2"
-         inkscape:deskcolor="#d1d1d1"
-         showguides="false"
-         inkscape:lockguides="false"><inkscape:grid
-       type="xygrid"
-       id="grid1046"
-       originx="0"
-       originy="0"
-       spacingy="1"
-       spacingx="1"
-       units="in"
-       visible="false" />\n""" % (width, height, width, height, width)
+    doc = f"""<?xml version='1.0' encoding='utf-8'?>
+<svg xmlns="http://www.w3.org/2000/svg"
+    xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+    xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+    width="{width}"
+    height="{height}"
+    viewBox="0 0 528 816"
+    version="1.1"
+    id="svg5"
+    inkscape:version="1.2.2 (b0a8486541, 2022-12-01)"
+    xml:space="preserve">
+    <defs id="defs21" />
+    <sodipodi:namedview id="namedview7" pagecolor="#ffffff" bordercolor="#666666" borderopacity="1.0" inkscape:pageshadow="2" inkscape:pageopacity="0.0" inkscape:pagecheckerboard="0" inkscape:document-units="in" inkscape:snap-global="false" fit-margin-top="0" fit-margin-left="0" fit-margin-right="0" fit-margin-bottom="0" units="in" width="528in" inkscape:showpageshadow="2" inkscape:deskcolor="#d1d1d1" showguides="false" inkscape:lockguides="false">
+    <inkscape:grid type="xygrid" id="grid1046" originx="0" originy="0" spacingy="1" spacingx="1" units="in" visible="false" />
+    """
 
+    tail = """
+    </sodipodi:namedview>
+</svg>"""
+
+    pages = []
     for i in range(n_pages):
-        doc += '<inkscape:page'
         if i == n_pages - 1:
-            doc += ' x="%f"' % (-width)
+            x = -width
         else:
-            doc += ' x="%f"' % ((i)*width + ((i/2) + (i%2)/2) * spacing)
-        doc += ' y="0"'
-        doc += ' width="%f"' % width
-        doc += ' height="%f"' % height
-        doc += ' id="page_%d"' % i
-        doc += ' margin="%f"' % margin
-        doc += ' bleed="0"/>\n'
+            x = (i) * width + ((i / 2) + (i % 2) / 2) * spacing
+        y = 0
+        width = width
+        margin = margin
 
-    doc += '</svg>'
+        pages.append(
+            f"""<inkscape:page x="{x}" y="{y}" width="{width}" height="{height}" id="page_{i}" margin="{margin}" bleed="0" />"""
+        )
 
-    with open(f_name, 'w') as f:
-        f.write(doc)
+    doc = doc + "\n".join(pages) + tail
+
+    # nice XML rendering instead of ns0:tag, ns1:tag etc.
+    ET.register_namespace("", "http://www.w3.org/2000/svg")
+    ET.register_namespace("inkscape", "http://www.inkscape.org/namespaces/inkscape")
+    ET.register_namespace(
+        "sodipodi", "http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+    )
+    root = ET.fromstring(doc)
+    tree = ET.ElementTree(root)
+    ET.indent(tree, space="  ")
+    tree.write(f_name, encoding="utf-8", xml_declaration=True)
+
 
 def nup(input_pdf_path, output_pdf_path, rows=1, cols=2):
-    is_landscape=True
+    is_landscape = True
     reader = PdfReader(input_pdf_path)
     writer = PdfWriter()
 
     num_pages = len(reader.pages)
-    pages_per_sheet = rows*cols
+    pages_per_sheet = rows * cols
 
     booklet_indices = []
-    for i in range(int(len(reader.pages)/4)):
-        booklet_indices.append(i*2)
-        booklet_indices.append(len(reader.pages)-i*2-1)
-        booklet_indices.append(i*2+1)
-        booklet_indices.append(len(reader.pages)-i*2-2)
+    for i in range(int(len(reader.pages) / 4)):
+        booklet_indices.append(i * 2)
+        booklet_indices.append(len(reader.pages) - i * 2 - 1)
+        booklet_indices.append(i * 2 + 1)
+        booklet_indices.append(len(reader.pages) - i * 2 - 2)
 
     if is_landscape:
         width = LETTER_HEIGHT
@@ -103,7 +99,11 @@ def nup(input_pdf_path, output_pdf_path, rows=1, cols=2):
         page_width = width / cols
         page_height = height / rows
 
-        positions = [(col * page_width, (rows - 1 - row) * page_height) for row in range(rows) for col in range(cols)]
+        positions = [
+            (col * page_width, (rows - 1 - row) * page_height)
+            for row in range(rows)
+            for col in range(cols)
+        ]
 
         for j in range(pages_per_sheet):
             if i + j < num_pages:
@@ -123,7 +123,14 @@ def nup(input_pdf_path, output_pdf_path, rows=1, cols=2):
 
                     # Translate the page to the correct position
                     position = positions[j]
-                    translation_matrix = [1, 0, 0, 1, position[0] + x_offset, position[1] + y_offset]
+                    translation_matrix = [
+                        1,
+                        0,
+                        0,
+                        1,
+                        position[0] + x_offset,
+                        position[1] + y_offset,
+                    ]
                     page.mediabox = RectangleObject([0, 0, page_width, page_height])
                     new_page.merge_transformed_page(page, translation_matrix)
                 else:
@@ -133,7 +140,14 @@ def nup(input_pdf_path, output_pdf_path, rows=1, cols=2):
 
                     # Translate the page to the correct position
                     position = positions[j]
-                    translation_matrix = [-1, 0, 0, -1, page_width + position[0] + x_offset, page_height + position[1] + y_offset]
+                    translation_matrix = [
+                        -1,
+                        0,
+                        0,
+                        -1,
+                        page_width + position[0] + x_offset,
+                        page_height + position[1] + y_offset,
+                    ]
                     page.mediabox = RectangleObject([0, 0, page_width, page_height])
                     new_page.merge_transformed_page(page, translation_matrix)
 
@@ -143,31 +157,55 @@ def nup(input_pdf_path, output_pdf_path, rows=1, cols=2):
 
 def cli():
     parser = argparse.ArgumentParser(
-                        prog='zinescape',
-                        description='Utilities for making zines',
-                        epilog='epilog here')
+        prog="zinescape", description="Utilities for making zines", epilog="epilog here"
+    )
+    subparsers = parser.add_subparsers(dest="command", required=True)
 
-    parser.add_argument('command')
-    parser.add_argument('-W', '--width')
-    parser.add_argument('-H', '--height')
-    parser.add_argument('-N', '--n_pages')
-    parser.add_argument('-R', '--rows', default=1)
-    parser.add_argument('-C', '--cols', default=2)
-    parser.add_argument('filename')
-    parser.add_argument('out_fn', nargs='?')
+    # compile command
+    compile_parser = subparsers.add_parser(
+        "compile", help="Compile PDF pages into a booklet"
+    )
+    compile_parser.add_argument("filename", help="Input PDF file")
+    compile_parser.add_argument("out_fn", nargs="?", help="Output PDF file (optional)")
+    compile_parser.add_argument(
+        "-R", "--rows", type=int, default=1, help="Number of rows (default: 1)"
+    )
+    compile_parser.add_argument(
+        "-C", "--cols", type=int, default=2, help="Number of columns (default: 2)"
+    )
+
+    # template command
+    template_parser = subparsers.add_parser(
+        "template", help="Create a template SVG file"
+    )
+    template_parser.add_argument("filename", help="Output template filename")
+    template_parser.add_argument(
+        "-N", "--n_pages", type=int, required=True, help="Number of pages in template"
+    )
+    template_parser.add_argument(
+        "-W", "--width", type=float, default=5.5, help="Width in inches (default: 5.5)"
+    )
+    template_parser.add_argument(
+        "-H",
+        "--height",
+        type=float,
+        default=8.5,
+        help="Height in inches (default: 8.5)",
+    )
 
     args = parser.parse_args()
-    print(args)
 
-    if args.command == 'compile':
+    if args.command == "compile":
         out_fn = args.out_fn
-        if out_fn == None:
-            out_fn = args.filename.split('.')[0] + '.compiled.' + args.filename.split('.')[1]
+        if out_fn is None:
+            parts = args.filename.split(".")
+            if len(parts) > 1:
+                out_fn = f"{parts[0]}.compiled.{parts[1]}"
+            else:
+                out_fn = f"{args.filename}.compiled"
         nup(args.filename, out_fn, args.rows, args.cols)
+    elif args.command == "template":
+        template(args.filename, args.n_pages, args.width, args.height)
 
-    if args.command == 'template':
-        if args.width == None:
-            args.width=5.5
-        if args.height == None:
-            args.height=8.5
-        template(args.filename, int(args.n_pages), float(args.width), float(args.height))
+    else:
+        parser.print_help()
